aefa316024f22c4d0f210892a66a1dabb5096834
raphaaaontop <raphaelazar11@gmail.com>
2026-05-11 22:11:50 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE = 'wallet-v1';
-const ASSETS = ['/', '/index.html', '/manifest.json'];
+const ASSETS = ['/flous/', '/flous/index.html', '/flous/manifest.json'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
